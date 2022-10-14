b7e785b0ca13d24dae0b9cd1447a45f09ef0d1f5
gmluqa <gmluqa@protonmail.com>
2022-10-14 13:36:13 +0200

    commit

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const app = document.getElementById("app")
 const url = "https://rickandmortyapi.com/api/character"
 
 
-const showResult = (resultado => console.log(resultado))
-const renderResult = (result) => {
-    console.log(result)
+const showResult = result => console.log(result)
+// accessing json objects propeties with .
+const extractData = result => result.data.results
+const clearApp = () => app.innerHTML = ""
 
+const renderResult = characters => {
+    clearApp()
+    characters.map(() => {
+        // document.createElement('button')(app)
+        let button = app.appendChild(document.createElement("button"))
+        app.appendChild(button)
+    })
 }
 
 app.innerHTML = "datos cargando..."
 
 axios
     .get(url)
+    .then(extractData)
     .then(renderResult)
